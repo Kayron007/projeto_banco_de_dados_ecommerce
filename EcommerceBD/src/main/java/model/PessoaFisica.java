e32b1dc744ff8b0c2ebd0079b76ddb2b5c232d26
Lunare-dot <matheusalizsouza@gmail.com>
2025-11-14 15:11:45 -0300

    feat(model): adiciona métodos de validação e normalização na classe PessoaFisica
    - adiciona construtor completo para PessoaFisica sem cadastro
    - adiciona métodos de validação para reforçar a integridade dos dados da entidade
    - adiciona métodos de normalização para padronizar entrada de dados
    - adiciona comentários detalhados para maior clareza e legibilidade do código
    - atualiza o método toString para exibir dados relevantes da PessoaFisica

--- EcommerceBD/src/main/java/model/PessoaFisica.java
+++ EcommerceBD/src/main/java/model/PessoaFisica.java
@@ -13,10 +13,29 @@ public class PessoaFisica extends EntidadeBase{
     private String cpf;
     private String rg;
 
+    /*
+     * Construtor padrão
+     * Utilizado para criar uma PessoaFisica que será populada depois;
+     */
     public PessoaFisica() {
         super();
     }
 
+    /*
+     * Construtor completo para PessoaFisica sem cadastro
+     * Utilizado para cadastrar uma nova PessoaFisica
+     * O ID será gerado automáticamente pelo método gerarIdUnico;
+     */
+    public PessoaFisica(Cliente id_cliente, String cpf, String rg) {
+        super();
+        this.id_cliente = id_cliente;
+        this.cpf = cpf;
+        this.rg = rg;
+    }
+
+    /*
+     * Construtor completo para PessoaFisica já cadastrado;
+     */
     public PessoaFisica(Long id, Cliente id_cliente, String cpf, String rg) {
         super(id);
         this.id_cliente = id_cliente;
@@ -24,12 +43,68 @@ public class PessoaFisica extends EntidadeBase{
         this.rg = rg;
     }
 
+    /* MÉTODOS DE VALIDAÇÃO */
+
     /*
-     * Completar mais tarde
+     * Valida todos os dados da PessoaFisica
+     * Lança exceções com mensagens específicas para cada erro;
+     */
+    public void validar() {
+        validarIdCliente();
+        validarCpf(cpf);
+        validarRG(rg);
+    }
+
+    /*
+     * Verifica se o id_cliente é nulo;
+     */
+    private void validarIdCliente() {
+        if(id_cliente == null) {
+            throw new IllegalArgumentException("ERRO: Cliente não encontrado!");
+        }
+    }
+
+    /*
+     * Chama a classe Validador para validar o CPF da PessoaFisica;
+     */
+    private void validarCpf(String cpf) {
+        this.cpf = Validador.utilCPF(cpf);
+    }
+
+    /*
+     * Chama a classe Validador para validar o RG da PessoaFisica;
+     */
+    private void validarRG(String rg) {
+        this.rg = Validador.utilRG(rg);
+    }
+
+    /* MÉTODO DE NORMALIZAÇÃO */
+    public void normalizar() {
+        /*
+         * Remove os espaços extras do CPF;
+         */
+        if(cpf != null)  {
+            cpf = cpf.trim();
+        }
+
+        /*
+         * Remove os espaços extras do RG;
+         */
+        if(rg != null) {
+            rg = rg.trim();
+        }
+    }
+
+    /*
+     * Exibe os dados da PessoaFisica;
      */
     @Override
     public String toString() {
-        return "";
+        return "Pessoa Física: {" +
+        "\nNome: " + (id_cliente.getNome()) +
+        "\nCPF: " + cpf +
+        "\nRG: " + rg +
+        "\n }";
     }
 
     /**
